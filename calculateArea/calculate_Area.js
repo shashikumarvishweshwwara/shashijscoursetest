8ace05c5dbf42d61a150ee4d72af8ac0f3d54d65
shashikumarvishweshwwara <shashikumarv2003@yahoo.co.in>
2024-03-20 04:35:01 -0400

    javascript functions

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,10 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+   function totalExpenditure() {
+    grocery1 = parseInt(document.getElementById('grocery1').value);
+    grocery2 = parseInt(document.getElementById('grocery2').value);
+    grocery3 = parseInt(document.getElementById('grocery3').value);
+    let totalexpenditure = grocery1 + grocery2 + grocery3;
+    document.getElementById('totalexpenditureresult').innerText = `The total expenditure is: ${totalexpenditure}`;
+   }   
